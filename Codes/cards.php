da0e7a15fc323ce312512029fcf77350d97122cb
kdhyani502 <59726961+KhushbooDhyani@users.noreply.github.com>
2021-04-20 11:03:22 +0530

    initial commit

---
+++ Codes/cards.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cards</title>
+    <link rel="stylesheet" href="cards.css">
+</head>
+<body>
+    <div class="row">
+    <a href="register.php">
+        <div class="column">
+            <div class="card">
+                <img src="Images/teacherbg.png" alt="Teacher" style="width:100%">
+                <h2>Teacher</h2>
+            </div>
+        </div>
+    </a>
+
+    <a href="login.php">
+        <div class="column">
+            <div class="card">
+            <img src="Images/adminbg.png" alt="Admin" style="width:100%">
+            <h2>Admin</h2>
+            </div>
+        </div>
+    </a>
+     
+    <a href="register.php">
+        <div class="column">
+            <div class="card">
+            <img src="Images/default.jpg" alt="Student" style="width:100%">
+            <h2>Student</h2>
+            </div>
+        </div>
+    </a>
+    </div>
+</body>
+</html>
